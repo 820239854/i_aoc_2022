56e2a90b4388c2164282cc51a89a8f8b4c029be9
Xing <820239854@qq.com>
2022-12-13 20:46:18 +0800

    day1 Q2 Complete!

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const input = fs.readFileSync("input.txt", {encoding: 'utf8', flag: 'r'});
 const inputArr = input.split("\n")
     .map(str => str.substring(0, str.length - 1))
-    .map(str => Number.parseInt(str));
+    .map(str => parseInt(str));
 let groupArr = [];
 let currArr = [];
 for (let i = 0; i < inputArr.length; i++) {
@@ -14,14 +14,10 @@ for (let i = 0; i < inputArr.length; i++) {
         currArr = [];
     }
 }
-let goroupCount = groupArr.map(arr => {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-        count += arr[i];
-    }
-    return count;
-})
-
-const max = Math.max(...goroupCount)
-
-console.log(max)
+let groupCount = groupArr.map(arr => arr.reduce((sum, num) => sum + num, 0))
+let sortedArr = groupCount.sort((a,b)=> b-a);
+let res =0;
+for(let i=0; i<3;i++){
+    res += sortedArr[i];
+}
+console.log(res)
